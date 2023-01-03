95a75537c3b99d45622cc76d0b500c8b10a43af5
Itahand Naizir <itahand.naizir@gmail.com>
2023-01-03 12:29:50 -0600

    Deleted everything to start over.

--- Blockversity/src/Flow/Scripts/get_all_addresses.ts
+++
@@ -1,9 +0,0 @@
-export const getAllAddresses = () => {
-  return `
-  import Guestbook from 0x800a10d0fff7acd4
-  pub fun main():[Address] {
-    let timestamp = Guestbook.addressInfo.keys
-    return timestamp
-  }
-  `
-}
--- Blockversity/src/components/index.ts
+++
@@ -1,3 +0,0 @@
- export { default as Navbar } from './Navbar';
- export { default as Welcome } from './Welcome';
- export { default as Footer } from './Footer';
